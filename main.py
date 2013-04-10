5cbbb3d19a60884f353362e412b791a81e1e0b3c
Migalin Danila <miga@thanatos.migalin.net>
2013-04-11 01:22:11 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.internet import reactor
+
+class chess_child(Resource):
+    isLeaf = True;
+
+class chess(Resource):
+    isLeaf = False;
+    def render_GET(self, request):
+        print request
+        return "<html><body>%s</body></html>" % request.uri
+    def getChild(self, path, request):
+        return chess_child(path)
+
+
+resource = chess()
+factory = Site(resource)
+reactor.listenTCP(8088, factory)
+reactor.run()
